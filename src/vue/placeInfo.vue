df25a3c750ceb6069d777904a4233c6b4a1a9179
seetheduck <lwj0506@naver.com>
2024-09-21 12:40:28 +0900

    리뷰 줄바꿈 가능

--- src/vue/placeInfo.vue
+++ src/vue/placeInfo.vue
@@ -136,6 +136,7 @@
                   v-model="newReview.contents"
                   maxlength="300"
                   required
+                  style="white-space: pre-wrap;"
                 ></textarea>
                 <p>{{ newReview.contents.length }} / 300</p>
               </div>
@@ -194,7 +195,7 @@
                   </div>
                 </div>
                 <div class="reviewText">
-                  <p v-if="review.contents">{{ review.contents }}</p>
+                  <p v-if="review.contents" style="white-space: pre-wrap;">{{ review.contents }}</p>
                   <p v-if="review.blocked">이 리뷰는 차단되었습니다.</p>
 
                   <!-- 좋아요 버튼 및 좋아요 수 -->
